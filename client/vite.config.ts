259c710bd185b911e724e83b3d2c83489aa2bacd
folk <folkza1755@gmail.com>
2025-09-03 14:16:26 +0700

    sadasd

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,     // ให้เข้าถึงจากเครื่องอื่นได้ (ถ้าต้องการ)
+    port: 5173
+  },
+  preview: {
+    port: 4173
+  }
 })
